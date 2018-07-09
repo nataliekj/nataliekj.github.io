ee636ed1c9f92244b57cec713cbd84727f3e048f
nataliekj <41002392+nataliekj@users.noreply.github.com>
2018-07-09 15:30:44 +0200

    My first Commit
    White Innovate for Children with ellipse

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(375,375);// put setup code here
+}
+
+function draw() {
+  // put drawing code here
+  background (0,153,255)
+// this circle will follow my mouse
+ellipse(175,175,175,40);
+fill("white");
+textSize (32);
+text ("Innovate for Children", 25, 75);
+}
